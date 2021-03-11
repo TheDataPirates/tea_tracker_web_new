9b4ffe10baf1f625ab094455e83d8eb101352d81
devin989 <deveenrath@gmail.com>
2021-03-12 00:47:17 +0530

    supplier details search is done

--- src/reports/pages/Purchasing.js
+++ src/reports/pages/Purchasing.js
@@ -9,7 +9,10 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import background from "../../assets/bg15.png";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
-import './SupplierProfile.css';
+import './SupplierSelection.css';
+import Input from "../../shared/components/FormElements/Input";
+import {VALIDATOR_REQUIRE} from "../../shared/util/validators";
+import {useForm} from "../../shared/hooks/form-hook";
 
 const useStyles = makeStyles(theme => ({
 
@@ -34,7 +37,8 @@ const useStyles = makeStyles(theme => ({
     root: {
         marginLeft: "5em",
         marginRight: "5em",
-    }
+    },
+
 }));
 
 const Purchasing = () => {
@@ -43,7 +47,25 @@ const Purchasing = () => {
 
     const {isLoading, error, sendRequest, clearError} = useHttpClient();
     const [loadedSuppliers, setLoadedSuppliers] = useState();
-    const [loadedSelectSuppliers, setLoadedSelectSuppliers] = useState();
+    // const [loadedSelectSuppliers, setLoadedSelectSuppliers] = useState();
+    // let cloneSuppliers;
+    const [formState, inputHandler] = useForm(
+        {
+            id: {
+                value: '',
+                isValid: false
+            },
+            name: {
+                value: '',
+                isValid: false
+            },
+            type:{
+                value:'',
+                isValid:false
+            }
+        },
+        false
+    );
 
 
     useEffect(() => {
@@ -54,43 +76,120 @@ const Purchasing = () => {
                 );
 
                 setLoadedSuppliers(responseData.suppliers);
+                // setLoadedSelectSuppliers(...loadedSuppliers);
+
                 console.log(responseData.suppliers);
-                // console.log(loadedSuppliers);
-                console.log("called");
+
+
 
             } catch (err) {
             }
         };
+
         fetchSuppliers();
+
     }, [sendRequest]);
 
-    // const supplierProfileHandler = async (id) => {
-    //
-    //     // setShowConfirmModal(false);
-    //     try {
-    //         const responseData =   await sendRequest(
-    //             `http://localhost:8080/supp/suppliers/${id}`,
-    //
-    //         );
-    //         setLoadedSelectSuppliers(responseData.supplier);
-    //         console.log(responseData.supplier);
-    //     } catch (err) {
-    //     }
-    // };
+    const supplierSelectHandler = async (event) => {
+        event.preventDefault();
+        console.log(formState.inputs);
+
+        if (formState.inputs.id.value !== '') {
+            setLoadedSuppliers(prevSupplier =>
+                prevSupplier.filter(supp => supp.supplier_id == formState.inputs.id.value)
+            );
+        }else if (formState.inputs.name.value !==''){
+            setLoadedSuppliers(prevSupplier =>
+                prevSupplier.filter(supp => supp.name === formState.inputs.name.value)
+            );
+        }else if (formState.inputs.type.value !==''){
+            setLoadedSuppliers(prevSupplier =>
+                prevSupplier.filter(supp => supp.type === formState.inputs.type.value )
+            );
+        }
+
+
+        // setShowConfirmModal(false);
+        // try {
+        //     const responseData =   await sendRequest(
+        //         `http://localhost:8080/supp/suppliers/${id}`,
+        //
+        //     );
+        //     setLoadedSelectSuppliers(responseData.supplier);
+        //     console.log(responseData.supplier);
+        // } catch (err) {
+        // }
+    };
+    const supplierResetHandler = async () => {
+        try {
+            const responseData = await sendRequest(
+                'http://localhost:8080/supp/suppliers'
+            );
+
+            setLoadedSuppliers(responseData.suppliers);
+            // setLoadedSelectSuppliers(...loadedSuppliers);
+
+            // console.log(responseData.suppliers);
+
+        } catch (err) {
+        }
+
+    }
 
 
     return (
-        <div className={classes.background} style={{height:"65em"}}>
-            <Grid container direction={"column"} spacing={3}>
+        <div className={classes.background} style={{height: "65em"}}>
+            <Grid container direction={"column"} spacing={3} alignItems={"center"}>
                 <ErrorModal error={error} onClear={clearError}/>
                 {isLoading && (
                     <div className="center">
                         <LoadingSpinner/>
                     </div>
                 )}
+
                 <Typography align={'center'} variant={'h2'}>Purchasing</Typography>
-                <Grid item style={{marginLeft:"5rem",marginRight:"5rem"}}>
-                    {!isLoading && loadedSuppliers && <Report items={loadedSuppliers}  header= {['ID','Supplier Name','Type','Telephone','Address','Status']}  />}
+                <Grid item >
+                    <form onSubmit={supplierSelectHandler} className={"supplier-profile-item__content"}>
+
+                        <Input
+                            id="id"
+                            element="input"
+                            type="text"
+                            label="Supplier ID :"
+                            validators={[]}
+                            errorText="Please enter a valid ID."
+                            onInput={inputHandler}
+                        />
+
+
+                        <Input
+                            id="name"
+                            element="input"
+                            type="text"
+                            label="Supplier Name :"
+                            validators={[]}
+                            errorText="Please enter a valid ID."
+                            onInput={inputHandler}
+                        />
+                        <Input
+                            id="type" element="dropdown" label="Supplier Type :" onInput={inputHandler}
+                            dropdownItems={["Grower Direct", "Grower through Agent","Dealer"]} validators={[]}
+                        />
+
+
+                        <Button type="submit">
+                            SEARCH
+                        </Button>
+                        <Button onClick={supplierResetHandler}>
+                            RESET
+                        </Button>
+
+
+                    </form>
+                </Grid>
+                <Grid item style={{marginLeft: "5rem", marginRight: "5rem",minWidth:"70rem"}}>
+                    {!isLoading && loadedSuppliers && <Report items={loadedSuppliers}
+                                                              header={['ID', 'Supplier Name', 'Type', 'Telephone', 'Address', 'Status']}/>}
                 </Grid>
 
             </Grid>
